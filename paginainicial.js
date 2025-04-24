f9ef547e5a6786df08b2fb3573794b3977efc247
Paulo Fontes <prof.paulo.e@gmail.com>
2025-04-23 23:13:31 -0300

    números importantes atualizados

--- paginainicial.js
+++ paginainicial.js
@@ -25,3 +25,84 @@ document.addEventListener('DOMContentLoaded', function() {
    
     cycleTexts();
 });
+
+// numeros importantes inicio
+
+
+function animateCounters() {
+    
+    const counters = document.querySelectorAll('.counter-value');
+    
+    
+    const animationDuration = 2000;
+    
+    
+    counters.forEach(counter => {
+      
+      const target = +counter.getAttribute('data-target');
+      
+      
+      let currentValue = 0;
+      counter.innerText = currentValue;
+      
+
+      const increment = target / (animationDuration / 16);
+      
+      
+      const updateCounter = () => {
+        
+        if (currentValue < target) {
+          
+          currentValue += increment;
+          
+          
+          if (currentValue > target) {
+            currentValue = target;
+          }
+          
+          
+          counter.innerText = Math.floor(currentValue);
+          
+          
+          requestAnimationFrame(updateCounter);
+        } else {
+          
+          counter.innerText = target;
+        }
+      };
+      
+      
+      requestAnimationFrame(updateCounter);
+    });
+  }
+  
+  
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom >= 0
+    );
+  }
+  
+  
+  let animationTriggered = false;
+  window.addEventListener('scroll', () => {
+    
+    const counterSection = document.querySelector('.counter-section');
+    
+    
+    if (counterSection && isElementInViewport(counterSection) && !animationTriggered) {
+      animateCounters();
+      animationTriggered = true;
+    }
+  });
+  
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const counterSection = document.querySelector('.counter-section');
+    if (counterSection && isElementInViewport(counterSection)) {
+      animateCounters();
+      animationTriggered = true;
+    }
+  });
